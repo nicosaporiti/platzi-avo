161d0c8d2ac02aeb2b0489c8e2674ce20094b5b9
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2020-10-22 23:06:19 -0300

    api del proyecto

---
+++ pages/ProductPage.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+
+const ProductPage = () => {
+  const [product, setProduct] = useState<TProduct>()
+
+  const {
+    query: { id },
+  } = useRouter()
+
+  useEffect(() => {
+    if (id) {
+      window
+        .fetch(`/api/avo/${id}`)
+        .then((res) => res.json())
+        .then((data) => setProduct(data))
+    }
+  }, [id])
+
+  console.log(product)
+
+  return (
+    <section>
+      <h1>Página producto: {product?.name}</h1>
+    </section>
+  )
+}
+
+export default ProductPage
