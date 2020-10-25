7b4962a2b375d03887bc070fe1aa5a963e7bb531
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2020-10-25 10:12:11 -0300

    getStaticProps y getStaticPaths

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,24 +1,44 @@
 import React, { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
 
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
+// Esta es una página dinámica pero en producción next debe saber cuáles páginas deben generarse
 
-  useEffect(() => {
-    if (query.id) {
-      window
-        .fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
-    }
-  }, [query.id])
+export const getStaticPaths = async () => {
+  const response = await fetch('https://platzi-avo-swart.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
 
+  const paths = productList.map(({ id }) => ({
+    params: {
+      id,
+    },
+  }))
+
+  return {
+    paths,
+    // incremental static generation
+    // Genera 404 por cualquier acceso fuera del API
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string
+  const response = await fetch(
+    `https://platzi-avo-swart.vercel.app//api/avo/${id}`
+  )
+  const product: TProduct = await response.json()
+
+  return {
+    props: {
+      product,
+    },
+  }
+}
+
+const ProductPage = ({ product }: { product: TProduct }) => {
   return (
     <Layout>
       {product == null ? null : <ProductSummary product={product} />}
